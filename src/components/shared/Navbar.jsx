15092c3b5a58df4f8bc68e7ddab37abab85ab829
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-02-20 11:51:53 +0600

    footer added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -27,8 +28,10 @@ const Navbar = () => {
 
     ]
     return (
-        <div className="navbar bg-base-100">
-        <div className="navbar-start">
+
+        <div className=" bg-gray-200">
+          <div className='navbar container mx-auto'>
+          <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
@@ -46,38 +49,30 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow m-3">
+             {
+              navItems.map((item)=>(
+                <Link className='font-bold m-3 hover:text-primary'href={item.path} key={item.path}>{item.title}</Link>
+              ))
+             }
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+         <Link href="/"> <Image src="logo.svg" height={50} width={50} alt='logoPic'></Image></Link>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-            <li><a>Item 3</a></li>
+        <div className="navbar-center hidden lg:flex ">
+          <ul className="menu menu-horizontal px-1 m-3">
+          {
+              navItems.map((item)=>(
+                <Link className='font-bold m-3 hover:text-primary' href={item.path} key={item.path}>{item.title}</Link>
+              ))
+             }
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn btn-outline btn-primary">Appointment</a>
         </div>
+          </div>
+       
       </div>
     );
 };
