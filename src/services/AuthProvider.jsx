3f27f7beb1984dd4bfc358277d052ebf81d5aaff
Jewel Hasan <jewelhasan.eee@gmail.com>
2025-02-23 09:12:47 +0600

    M80-9 SignIn problems solve  options.provider is not iteratble.

--- src/services/AuthProvider.jsx
+++ src/services/AuthProvider.jsx
@@ -1,11 +1,14 @@
 "use client"
+import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 
 const AuthProvider = ({children}) => {
     return (
-        <div>
-        {children}
-        </div>
+        
+            <SessionProvider>
+            {children}
+            </SessionProvider>
+      
     );
 };
 
