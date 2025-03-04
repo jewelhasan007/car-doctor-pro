6384c3a3321a6f957bda1db7402d6679b1fd8acd
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-03-04 12:31:14 +0600

    M81-7 My booking api added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -8,6 +8,7 @@ import GitHubProvider from "next-auth/providers/github";
 
 
 export const authInfo = {
+    secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
 session : {
     strategy : "jwt",
     maxAge : 30 * 24 * 60 * 60
