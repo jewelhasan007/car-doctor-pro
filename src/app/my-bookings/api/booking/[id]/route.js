e84c6a817b879c7876b2c666a14b0db8cbecf2a2
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-03-07 00:57:17 +0600

    M81-11 Patch API for Update

--- src/app/my-bookings/api/booking/[id]/route.js
+++ src/app/my-bookings/api/booking/[id]/route.js
@@ -14,17 +14,29 @@ export const DELETE = async (request, {params}) =>{
     }
 }
 
-// export const PATCH = async (request, {params}) =>{
-//     const db = await connectDB();
-//     const bookingCollection = db.collection('new-bookings')
-//     try {
-//        const resp = await bookingCollection.deleteOne({_id: new ObjectId(params.id)});
+export const PATCH = async (request, {params}) =>{
+    const db = await connectDB();
+    const bookingCollection = db.collection('new-bookings')
+    const {date, phone, address} = await request.json()
+    try {
+       const resp = await bookingCollection.updateOne({_id: new ObjectId(params.id)},
+    {
+        $set : {
+            date,
+            phone,
+            address
+        }
+    },
+    {
+        upsert : true
+    }
+);
 
-//        return Response.json({message: "deleted the bookings", response : resp})
-//     } catch (error) {
-//         return Response.json({message: "something went wrong"})
-//     }
-// }
+       return Response.json({message: "updated the bookings", response : resp})
+    } catch (error) {
+        return Response.json({message: "something went wrong"})
+    }
+}
 
 export const GET = async (request, {params}) =>{
     const db = await connectDB();
